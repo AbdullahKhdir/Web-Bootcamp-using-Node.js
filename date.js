f1b3ebfef03de28554265c5005bda63dbaa39d17
AbdullahKhdir <abdullahkhder77@gmail.com>
2021-01-02 16:57:30 +0100

    deploy todolist app on heroku ready files

---
+++ date.js
@@ -0,0 +1,23 @@
+//Use Modules to a better reuseabilty and refactor the code
+//console.log(module);
+exports.getDate= function() {
+  let today = new Date();
+  let currentDay = today.getDay(); // 0 Sunday 1,2,3,4,5 6saturday
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US" , options);
+}
+
+exports.getDay=function(){
+  let today = new Date();
+  let currentDay = today.getDay(); // 0 Sunday 1,2,3,4,5 6saturday
+  let options = {
+    weekday: "long"
+  };
+
+  return "Today is "+today.toLocaleDateString("en-US" , options);
+}
