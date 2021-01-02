7f24baa89ecf732d3110c8b8abf0d609547fe7c9
AbdullahKhdir <abdullahkhder77@gmail.com>
2021-01-02 17:51:40 +0100

    Added a Procfile

--- app.js
+++ app.js
@@ -135,6 +135,11 @@ app.get("/:dynPage", function(req, res){
   });
 });
 
-app.listen(process.env.PORT ||3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server is on !");
 });
